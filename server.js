23a8d8a9f2c258d55924e842050f49d4edab56ed
priyadharshanexe <priyadharshan.exe@gmail.com>
2026-06-30 15:52:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/plain" });
@@ -10,10 +12,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = 3000;
-
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = server;
