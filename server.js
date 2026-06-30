a65bb2a84f3bc261526c876f90b4fec5f4042f42
priyadharshanexe <priyadharshan.exe@gmail.com>
2026-06-30 16:20:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`
-        <h1>Node.js Server Running</h1>
-        <p>Hosted Successfully</p>
-    `);
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Node.js Server is Running");
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+    }
 });
 
 server.listen(PORT, "0.0.0.0", () => {
