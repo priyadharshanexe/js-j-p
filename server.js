601b64d02a039befead40e3a66fbc43bcd71f890
priyadharshanexe <priyadharshan.exe@gmail.com>
2026-06-30 16:35:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,41 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Node.js Server is Running");
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("Not Found");
-    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(`
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Node.js Web Server</title>
+    <style>
+        body{
+            font-family:Arial,sans-serif;
+            text-align:center;
+            margin-top:100px;
+            background:#f4f4f4;
+        }
+        h1{
+            color:#2c3e50;
+        }
+        p{
+            font-size:20px;
+        }
+    </style>
+</head>
+<body>
+
+<h1>🚀 Node.js Server is Running</h1>
+
+<p>This page is hosted using Node.js.</p>
+
+<p>Jenkins Pipeline Deployment Successful.</p>
+
+</body>
+</html>
+    `);
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
